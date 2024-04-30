015058c3dea6640db44e78f6c55fa01ff9857608
Jesus Castro <castrojesus39@gmail.com>
2024-04-29 18:06:53 -0700

    added two components TodoInput/TodoList plus placeholder data from an array

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
+// This is basically main similar to C#/Java
+// Highest level parent component 
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
+
 function App() {
+  let todos = [
+    "Go to the gym",
+    "Eat more fruits",
+    "Pick up the kids from school"
+  ]
   return (
     <>
-      <div></div>
+      <TodoInput />
+      <TodoList todos={todos} />
     </>
   )
 }
+
+export default App;

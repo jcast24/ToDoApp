cf692c402fbd7f98fad7231509b096480a95a21c
Jesus Castro <castrojesus39@gmail.com>
2024-04-29 20:01:52 -0700

    changed formatting to be easier to read

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,8 +1,23 @@
-export default function TodoInput() {
+export default function TodoInput(props) {
+  const { handleUpdateTodos, todoValue, setTodoValue } = props;
+
+  // onChange listens to changes made on the input
+  // whenever the value of the input changes, it calls this function
+  // which receives an event. 
+
+  // e.target.value is the new text that gets entered in the TodoInput
+
   return (
     <header>
-      <input placeholder="Enter todo"></input>
-      <button>Add</button>
+      <input
+        value={todoValue}
+        onChange={(e) => { setTodoValue(e.target.value) }}
+        placeholder="Enter todo"
+      />
+      <button onClick={() => {
+        handleUpdateTodos(todoValue)
+        setTodoValue('');
+      }}>Add</button>
     </header>
   )
 }

1884a5bd01610d6c6178a06e86f5c01e3ff18b4d
Jesus Castro <castrojesus39@gmail.com>
2024-04-29 20:01:15 -0700

    changed formatting to be easier to read

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -4,13 +4,21 @@ import TodoCard from './TodoCard'
 export default function TodoList(props) {
     const { todos } = props;
 
+    // We're passing in handleDeleteTodo, but we need it in TodoCard
+    // we pass in {...props} into TodoCard
+
     return (
         <ul className='main'>
-            {todos.map((todo, todoIndex) =>
-                <TodoCard key={todoIndex}>
-                    <p>{todo}</p>
-                </TodoCard>
-            )}
+            {todos.map((todo, todoIndex) => {
+                return (
+                    <TodoCard
+                        {...props}
+                        key={todoIndex}
+                        index={todoIndex}>
+                        <p>{todo}</p>
+                    </TodoCard>
+                )
+            })}
         </ul>
     )
 
